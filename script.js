6fa42b467e5c324e2ac03d570b0e86ce0a50987f
stacy <smartinez01.student@hsfi.us>
2017-03-06 22:11:04 +0000

    I made tiny turt turnt!

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function square(size){
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 }
 
 // Type your function call below
-
+square(90);
 
 stamp();
+
+function triangle(size){
+    forward(size);
+    right (120);
+    forward(size);
+    right(120);
+    forward(size);
+}
+triangle(80);
+
+forward(50);
+
+function rhombus(size){
+    forward(size);
+    right (100);
+    forward(size);
+    right(80);
+    forward(size);
+    right(80);
+    forward(size);
+    right(190);
+}
+rhombus(38);
+
+function hexagon(){
+    
+}
+
+hexagon();
+
+
+
